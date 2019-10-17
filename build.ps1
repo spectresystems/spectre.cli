2af39cbaa40c3083c033b401c592acdd3f6777fb
Patrik Svensson <patrik@patriksvensson.se>
2019-10-18 00:28:49 +0200

    Update Cake to version 0.35.

--- build.ps1
+++ build.ps1
@@ -27,7 +27,7 @@ if (!(Test-Path $Cakeup)) {
 }
 
 # Execute Cakeup
-&$Cakeup "run" "--cake=0.30.0" "--sdk=2.1.400" `
+&$Cakeup "run" "--cake=0.35.0" "--sdk=3.0.100" `
         "--execute" "--" $ScriptArgs
 
 # Return the exit code from Cakeup.
